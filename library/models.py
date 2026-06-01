581a7988db697159b1376eb0eb5ae63c88051a81
Vahidvhd <vahidvhd91@gmail.com>
2026-06-01 14:30:39 +0100

    Add Author and Book models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    authors = models.ManyToManyField(Author, related_name='books')
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
+    
+    def clean(self):
+        if len(self.isbn) != 13 or not self.isbn.isdigit():
+            raise ValidationError({'isbn':'Invalid ISBN'})
+
+    
